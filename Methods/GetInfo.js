ec94878022aa6dbdbf7c12ffa07f088a09ea4e71
Kirill Glukhov <valeralunin34@gmail.com>
2023-12-30 19:59:22 +0700

    Реализовал новый функционал
    - Теперь для пользователя есть больше возможностей взаимодействия с сайтом

---
+++ Methods/GetInfo.js
@@ -0,0 +1,34 @@
+import { getWithToken } from "../Methods/Methods.js";
+
+export async function getMainInformation(token, profile, dishesInCart = null)
+{
+    if (localStorage.getItem("tokenExpiry") && new Date().getTime() > parseInt(localStorage.getItem("tokenExpiry"))) 
+    {
+        localStorage.removeItem("token");
+        localStorage.removeItem("tokenExpiry");
+    }
+    else
+    {
+        token = localStorage.getItem("token");
+
+        if (token === null)
+        {
+            profile = null;
+        }
+        else
+        {
+            profile  = await getWithToken(`https://food-delivery.kreosoft.ru/api/account/profile`, token);
+
+            if (dishesInCart !== null)
+            {
+                dishesInCart = await getWithToken(`https://food-delivery.kreosoft.ru/api/basket`, localStorage['token']);
+            }
+        }
+    }
+
+    return {
+        "token": token,
+        "profile": profile,
+        "dishesInCart": dishesInCart
+    }
+}
